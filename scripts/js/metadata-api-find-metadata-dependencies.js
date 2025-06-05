e2d19a5f2a6987d629b356abfcc8ead6700a5dfb
Alberto Mata <alberto.mata@metasintaxis.com>
2025-06-04 22:06:18 -0600

    GH-8: Changing JSON path to totalSize in JS

--- scripts/js/metadata-api-find-metadata-dependencies.js
+++ scripts/js/metadata-api-find-metadata-dependencies.js
@@ -1,50 +1,50 @@
-import fs from "fs";
-import * as core from "@actions/core";
+import fs from 'fs';
+import * as core from '@actions/core';
 
 const readDependencies = (filePath) => {
-  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+	return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 };
 
 const buildDependencyTableData = (dependencies) => {
-  const records = dependencies.result.result.records || [];
-  const tableHeader = [
-    { data: "Source Name", header: true },
-    { data: "Source Type", header: true },
-    { data: "Target Name", header: true },
-    { data: "Target Type", header: true }
-  ];
-  const tableData = [tableHeader];
+	const records = dependencies.result.result.records || [];
+	const tableHeader = [
+		{ data: 'Source Name', header: true },
+		{ data: 'Source Type', header: true },
+		{ data: 'Target Name', header: true },
+		{ data: 'Target Type', header: true }
+	];
+	const tableData = [tableHeader];
 
-  for (const record of records) {
-    tableData.push([
-      { data: record.RefMetadataComponentName },
-      { data: record.RefMetadataComponentType },
-      { data: record.MetadataComponentName },
-      { data: record.MetadataComponentType }
-    ]);
-  }
-  return tableData;
+	for (const record of records) {
+		tableData.push([
+			{ data: record.RefMetadataComponentName },
+			{ data: record.RefMetadataComponentType },
+			{ data: record.MetadataComponentName },
+			{ data: record.MetadataComponentType }
+		]);
+	}
+	return tableData;
 };
 
 const writeDependencySummary = async (dependencyTableData, dependencies) => {
-  const DEPENDENCY_REPORT_HEADING = "Metadata Dependency Report";
-  const TOTAL_DEPENDENCIES_LABEL = `Total Dependencies: ${dependencies.result.totalSize}`;
+	const DEPENDENCY_REPORT_HEADING = 'Metadata Dependency Report';
+	const TOTAL_DEPENDENCIES_LABEL = `Total Dependencies: ${dependencies.result.result.totalSize}`;
 
-  await core.summary
-    .addHeading(DEPENDENCY_REPORT_HEADING)
-    .addRaw(TOTAL_DEPENDENCIES_LABEL)
-    .addBreak()
-    .addTable(dependencyTableData)
-    .write();
+	await core.summary
+		.addHeading(DEPENDENCY_REPORT_HEADING)
+		.addRaw(TOTAL_DEPENDENCIES_LABEL)
+		.addBreak()
+		.addTable(dependencyTableData)
+		.write();
 };
 
 const main = async () => {
-  const filePath = process.argv[2];
-  const dependencies = readDependencies(filePath);
-  const dependencyTableData = buildDependencyTableData(dependencies);
-  await writeDependencySummary(dependencyTableData, dependencies);
+	const filePath = process.argv[2];
+	const dependencies = readDependencies(filePath);
+	const dependencyTableData = buildDependencyTableData(dependencies);
+	await writeDependencySummary(dependencyTableData, dependencies);
 };
 
 main().catch((err) => {
-  core.setFailed(`Action failed with error: ${err}`);
+	core.setFailed(`Action failed with error: ${err}`);
 });
